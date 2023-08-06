f9cedf144f94b0c25ced89311685816b5c1c4c81
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-08-06 12:41:38 +0530

    login function with generating and sending user token

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -38,4 +38,31 @@ export const register = async (req, res) => {
     } catch (error) {
         res.status(500).json({error: error.message});
     }
+};
+
+/* LOGGING IN FUNCTION */
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({email: email});
+
+        if(!user){
+            return res.status(400).json({msg: "User does not exist"});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({msg: "Invalid Credentials"});
+        
+        }
+
+        const token = jwt.sign({id: user.id}, process.env.JWT_SCERET);
+        delete user.password;
+        res.send(200).json({ token, user });
+
+    } catch (error) {
+
+        res.status(500).json({error: error.message});
+        
+    }
 }
