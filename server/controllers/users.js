22c1246d82cbb8cbfe547de1c0be79cd48f4029f
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-08-06 14:05:01 +0530

    functions to get user and user friend details

---
+++ server/controllers/users.js
@@ -0,0 +1,34 @@
+import User from "../models/User";
+
+//READ
+export const getUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const getUserFriends = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findById(id);
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        );
+
+        const formattedfriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return {_id, firstName, lastName, occupation, location, picturePath };
+            }
+        );
+
+        res.status(200).json(formattedfriends);
+    }
+    catch(error) {
+        res.status(404).json({message: error.message});
+    }
+}
