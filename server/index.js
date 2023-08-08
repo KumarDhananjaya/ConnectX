89bd6480f329b21712acfd5ed9fe5343827b459c
S Kumar Dhananjaya <KUMARDHANANJAYA.EDU@GMAIL.COM>
2023-08-08 17:34:30 +0530

    imported dummy data and stored it in mongoDB

--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import { createPost } from "./controllers/posts.js"
 import { fileURLToPath } from "url";
 import { register } from "./controllers/auth.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
 
 /* CONGFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -49,6 +52,10 @@ mongoose.connect(process.env.MONGO_URL, {
   useUnifiedTopology: true,
 }).then(() => {
   app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+  User.insertMany(users);
+  Post.insertMany(posts);
+
 }).catch((error) => console.log(`${error} did not connect` ));
 
 /* ROUTES WITH FILES */
